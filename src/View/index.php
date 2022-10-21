c35394d1af234eb603434aa97cef81167223a681
izadoramatias <izadora.lima@ftdata.com.br>
2022-10-21 12:37:33 -0300

    initialized front-end

---
+++ src/View/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>IOManager</title>
+
+    <link rel="shortcut icon" href="assets/favicon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="style/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+    <div class="container">
+        <div class="content">
+
+            <nav>
+                <header>
+                    <div class="logo">
+                            <a href="">
+                            <img src="assets/logo.svg" alt="Logo IOManager">
+                        </a>
+                    </div>
+                    <div class="new__transaction">
+                        <button>
+                            Nova transação
+                        </button>
+                    </div>
+                </header>
+                <div class="io__data">
+                    <section class="input__data">
+                        <header>
+                            <p>
+                                <span class="io__description">Entradas</span>
+                                <span class="io__icon">
+                                <img src="assets/input-icon.svg" alt="Input money icon">
+                            </span>
+                            </p>
+                        </header>
+                        <div class="input__money">
+                            <span>R$</span>
+                            <span>16.400, 00</span>
+                        </div>
+                    </section>
+                    <section class="output__data">
+                        <header>
+                            <p>
+                                <span class="io__description">Saídas</span>
+                                <span class="io__icon">
+                                <img src="assets/output-icon.svg" alt="Output money icon">
+                            </span>
+                            </p>
+                        </header>
+                        <div class="output__money">
+                            <span>R$</span>
+                            <span>1.259, 00</span>
+                        </div>
+                    </section>
+                    <section class="total__data">
+                        <header>
+                            <p>
+                                <span class="io__description">Total</span>
+                                <span class="io__icon">
+                                <img src="assets/money-icon.svg" alt="Dollar icon">
+                            </span>
+                            </p>
+                        </header>
+                        <div class="total__money">
+                            <span>R$</span>
+                            <span>15.141, 00</span>
+                        </div>
+                    </section>
+                </div>
+            </nav>
+
+            <main></main>
+
+        </div>
+    </div>
+
+</body>
+</html>
